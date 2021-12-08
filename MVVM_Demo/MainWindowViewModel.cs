775bb66d3849da6210cc139ac88f385c39e92151
Eric Müller <de.muller.eric@gmail.com>
2021-12-08 18:52:02 +0100

    Projektdateien hinzufügen.

---
+++ MVVM_Demo/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Demo
+{
+    public class MainWindowViewModel
+    {
+        public Person Person { get; set; }
+        public List<Person> PersonList { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Person = new Person
+            {
+                Name = "Test",
+                Vorname = "Tester"
+            };
+
+            PersonList = new List<Person>
+            {                
+
+                new Person()
+                {
+                    Name = "Muster",
+                    Vorname = "Musterfrau"
+                },
+
+                new Person()
+                {
+                    Name = "Muster",
+                    Vorname = "Mustermann"
+                }
+            };
+
+
+        }
+
+        
+    }
+}
